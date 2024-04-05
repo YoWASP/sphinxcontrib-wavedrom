09a99577b860f3e12d762252672b224ce1a8cea9
Catherine <whitequark@whitequark.org>
2024-04-05 01:52:02 +0000

    Disable Jekyll in test documentation output.

--- test/conf.py
+++ test/conf.py
@@ -2,7 +2,8 @@ project = "WaveDrom extension for Sphinx"
 extensions = ["sphinxcontrib.yowasp_wavedrom"]
 master_doc = "index"
 html_theme = "furo" # with dark theme support
-html_static_path = ['_static']
+html_static_path = ["_static"]
 html_css_files = ["wavedrom.css"]
+html_extra_path = [".nojekyll"]
 
 # yowasp_wavedrom_skin = "default"
